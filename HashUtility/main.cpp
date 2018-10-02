878fd7cbbf18bec53e0020433ebde6fea5e8309a
Achoual Othmane <othmane.achoual@4d.com>
2018-10-02 16:44:49 +0100

    Initial Commit

---
+++ HashUtility/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashUtility
+//
+//  Created by Achoual Othmane on 02/10/2018.
+//  Copyright © 2018 Achoual Othmane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
